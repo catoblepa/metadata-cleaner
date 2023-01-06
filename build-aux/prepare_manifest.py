1d63e45ab2f01d9db690baf8d5ffc03aa5f9e420
Romain Vigier <romain@romainvigier.fr>
2023-01-06 18:26:37 +0100

    CI: Use different Flatpak branches for test builds and releases

--- build-aux/prepare_manifest.py
+++ build-aux/prepare_manifest.py
@@ -14,16 +14,22 @@ MANIFEST_FILE = "build-aux/fr.romainvigier.MetadataCleaner.yaml"
 def rewrite_manifest() -> None:
     """Rewrite manifest to remove the devel flag."""
     print("Rewriting manifest for release...")
+
     with open(MANIFEST_FILE, "r") as f:
         manifest = yaml.safe_load(f)
+
+    manifest["default-branch"] = "stable"
+
     try:
         opts = manifest["modules"][-1]["config-opts"]
     except KeyError:
         return
     opts = ["-Ddevel=false" if i == "-Ddevel=true" else i for i in opts]
     manifest["modules"][-1]["config-opts"] = opts
+
     with open(MANIFEST_FILE, "w") as f:
         yaml.dump(manifest, f)
+
     print("Finished rewriting manifest.")
 
 
